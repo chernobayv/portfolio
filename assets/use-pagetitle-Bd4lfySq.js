27857735cf3c9a1c5abd24c460b12d93bd703eb4
chernobayv <chernobayv@users.noreply.github.com>
2026-01-28 20:57:55 +0000

    [ci] deploy site 8b11bfc624b721bc498621234d047ed3125e12d0

---
+++ assets/use-pagetitle-Bd4lfySq.js
@@ -0,0 +1,1 @@
+import{r as t,M as s}from"./index-C-J3r2B0.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-COBbAX2a.js
+++
@@ -1,1 +0,0 @@
-import{r as t,M as s}from"./index-ChP8rvUS.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
